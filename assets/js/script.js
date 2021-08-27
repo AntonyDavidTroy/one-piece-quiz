97f3c9d4a0ef8025c33a5390af613528bc9e6e38
AntonyDavidTroy <antonydavidtroy@googlemail.com>
2021-08-27 18:25:55 +0000

    finish basic css styling and add some questions in Array

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+/** Array of Objects */
+
+let quiz = [
+    {
+        q:"Who was the first member to join Luffy's crew in the Straw Hat Pirates?",
+        options:["Zoro","Sanji","Nami","Usopp"],
+        answer:1
+    },
+    {
+        q:"Who is Luffy's father?",
+        options:["Gol D. Roger","Garp","Monkey D. Dragon","Shanks",],
+        answer:3
+    },
+    {
+        q:"What devil fruit did Luffy eat?",
+        options:["Mero Mero no Mi","Goro Goro no Mi","Yami Yami no Mi","Gomu-Gomu no Mi"],
+        answer:4
+    },
+    {
+        q:"How many Swords does Zoro wield?",
+        options:["1","2","3","4"],
+        answer:3
+    },
+    {
+        q:"How much was Luffy's first Bounty?",
+        options:["20,000,000 Berries","25,000,000 Berries","30,000,000 Berries","35,000,000 Berries"],
+        answer:3
+    },
+    {
+        q:"What did Shanks lose when he saved Luffy from drowning?",
+        options:["An arm","A leg","A foot","A hand"],
+        answer:1
+    },
+    {
+        q:"What is the correct order of highest to lowest bounties, after Enies Lobby Arc? ",
+        options:["Luffy, Zoro, Sanji, Robin, Usopp","Luffy, Sanji, Zoro, Robin, Usopp","Luffy, Zoro, Usopp, Sanjim Robin","Luffy, Zoro, Robin, Sanji, Usopp"],
+        answer:4
+    },
+]
