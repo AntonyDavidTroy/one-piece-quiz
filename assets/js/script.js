5b3794c15c6bd446d533da21a14606aedb76e180
AntonyDavidTroy <antonydavidtroy@googlemail.com>
2021-08-28 10:23:42 +0000

    Add and style Quiz rules box

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,47 @@ let quiz = [
     },
     {
         q:"What is the correct order of highest to lowest bounties, after Enies Lobby Arc? ",
-        options:["Luffy, Zoro, Sanji, Robin, Usopp","Luffy, Sanji, Zoro, Robin, Usopp","Luffy, Zoro, Usopp, Sanjim Robin","Luffy, Zoro, Robin, Sanji, Usopp"],
+        options:["Luffy, Zoro, Sanji, Robin, Usopp","Luffy, Sanji, Zoro, Robin, Usopp","Luffy, Zoro, Usopp, Sanji, Robin","Luffy, Zoro, Robin, Sanji, Usopp"],
         answer:4
+    },
+    {
+        q:"What Sea does Sanji Originally come from?",
+        options:["North Blue","East Blue","South Blue","West Blue"],
+        answer:1
+    },
+    {
+        q:"How many years was Brook stranded alone for afrer his crew died?",
+        options:["40","50","60","80"],
+        answer:3
+    },
+    {
+        q:"Who was the first member to reach the Thousand Sunny after the time-skip?",
+        options:["Zoro","Franky","Nami","Robin"],
+        answer:2
+    },
+    {
+        q:"How many Warlords has Luffy defeated by the Wano Arc?",
+        options:["6","4","5","7"],
+        answer:2
+    },
+    {
+        q:"How many different swords has Zoro wielded as of chapter 1,000?",
+        options:["7","9","10","12"],
+        answer:3
+    },
+    {
+        q:"How many times was Whitebeard shot with bullets during the Paramount War arc?",
+        options:["112","214","96","152"],
+        answer:4
+    },
+    {
+        q:"How many times has Red Haired Shanks appeared in the manga as of chapter 1,000?",
+        options:["18","15","22","25"],
+        answer:1
+    },
+    {
+        q:"What devil fruit does Sengoku have?",
+        options:["Mito Mito no Mi","Gito Gito no Mi","Jito Jito no Mi","Hito Hito no Mi"],
+        answer:1
     },
 ]
